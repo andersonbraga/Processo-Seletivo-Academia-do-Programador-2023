706a3c3f6746c915cf497df67531d01c897d5667
Anderson <anderson.braga@uniplaclages.edu.br>
2023-02-24 17:25:29 -0300

    Teste01

---
+++ Teste01/Triangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿ Console.WriteLine("Hello user. Can you write measurements from first side of triangles ? ");
+            double sideA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hello user. Can you write measurements from second side of trangles ? ");
+            double sideB = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hello user, Can you write measurements from third side of triangles ? ");
+            double sideC = Convert.ToDouble(Console.ReadLine());
+          
+
+            if ( sideA == sideB &&  sideA == sideB && sideB == sideC )
+                {
+                    Console.WriteLine("Equilateral " );
+                }
+            else if  (sideA == sideB || sideA == sideC || sideB == sideC)
+                {
+                    Console.WriteLine("Isosceles ");
+                }
+            else 
+                {
+                    Console.WriteLine("Escaleno");
+                    
+                }
